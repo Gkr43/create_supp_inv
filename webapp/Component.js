24cd5436276062ab5a8866219e0f97435e82eb50
Trupti Sandbhor <trupti.sandbhor@zencon.co.in>
2024-08-13 09:50:25 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("createsuppinv.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
